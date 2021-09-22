13046719ff61d80624ffdb4d8918f7e73b4c506a
Alban Jamon <jamon.alban@gmail.com>
2021-09-22 14:33:09 +0200

    Création du StaticController et de ses 3 méthodes

---
+++ app/Http/Controllers/StaticController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticController extends Controller
+{
+    public function home()
+    {
+        return view('home');
+    }
+
+    public function trips()
+    {
+        return view('trips');
+    }
+
+    public function trip($id)
+    {
+        return view('trip');
+    }
+}
