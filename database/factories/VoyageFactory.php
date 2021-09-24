5c59191e16055c1557f5ae4b838ff462f44a3c5b
Alban Jamon <jamon.alban@gmail.com>
2021-09-24 12:19:17 +0200

    Ajout et définition de Model, Factory, Migration pour Voyage

---
+++ database/factories/VoyageFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Voyage;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class VoyageFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Voyage::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'location' => $this->faker->country(),
+            'description' => $this->faker->sentence(),
+            'travelDate' => $this->faker->date('Y-m-d'),
+            'price' => $this->faker->randomFloat(2,500,9999),
+            'organizer' => $this->faker->company(),
+            'note' => $this->faker->numberBetween(0,5)
+        ];
+    }
+}
---
+++ database/migrations/2021_09_24_095250_create_voyages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVoyagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('voyages', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('location');
+            $table->string('description');
+            $table->date('travelDate');
+            $table->float('price',6,2,true);
+            $table->string('organizer');
+            $table->integer('note');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('voyages');
+    }
+}
